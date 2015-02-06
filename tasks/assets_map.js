034b5c9dd542c78929cf2495569fa5b32b4ad49b
Peter Bouquet <peter.bouquet@net-a-porter.com>
2015-02-06 19:01:43 +0000

    Add async/synchronous file write as option

--- tasks/assets_map.js
+++ tasks/assets_map.js
@@ -10,29 +10,40 @@
 
 var fs = require('fs');
 
+var _buildMap = function(grunt, opts){
+  var hashedAssets = grunt.file.expand(opts.paths),
+      assetMap = {},
+      prefixPath = opts.prefixPath || "";
+
+  for(var assetIndex in hashedAssets) {
+      var asset = prefixPath+hashedAssets[assetIndex].replace(opts.stripPath, "");
+      var splitAsset = asset.split('.');
+      if(splitAsset.length >= 2) {
+          splitAsset.splice(splitAsset.length-2, 1);
+          var unhashedName = splitAsset.join(".");
+          assetMap[unhashedName] = asset;
+      }
+  }
+  return JSON.stringify(assetMap);
+}
+
 module.exports = function(grunt) {
   grunt.registerTask('assets_map', 'Creates a map of assets to versioned assets', function(){
-      var done = this.async();
       var opts = this.options();
-      var hashedAssets = grunt.file.expand(opts.paths),
-          assetMap = {},
-          prefixPath = opts.prefixPath || "";
+      var fileName = opts.fileName || 'asset-hash-map.json';
 
-      for(var assetIndex in hashedAssets) {
-          var asset = prefixPath+hashedAssets[assetIndex].replace(opts.stripPath, "");
-          var splitAsset = asset.split('.');
-          if(splitAsset.length >= 2) {
-              splitAsset.splice(splitAsset.length-2, 1);
-              var unhashedName = splitAsset.join(".");
-              assetMap[unhashedName] = asset;
-          }
+      if(!opts.async){
+        fs.writeFileSync(fileName, _buildMap(grunt, opts));
+        grunt.log.writeln("Asset map written to '"+fileName+"' successfully");
+      }
+      else{
+        var done = this.async();
+        fs.writeFile(fileName, _buildMap(grunt, opts), function(err){
+          if(err) throw err;
+          grunt.log.writeln("Asset map written to '"+fileName+"' successfully");
+          done();
+        });
       }
 
-      var mapFileName = opts.fileName || 'asset-hash-map.json';
-      fs.writeFile(mapFileName, JSON.stringify(assetMap), function(err){
-          if(err) throw err;
-          grunt.log.writeln("Asset map written to '"+mapFileName+"' successfully");
-          done();
-      });
   });
 };
